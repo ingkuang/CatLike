715542b1d6e000298818a5fd79220a83f81a943d
ingkuang <jaesung741@naver.com>
2020-12-19 16:38:33 +0900

    first

---
+++ CatLike/Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    public float scrollSpeed = 0.05f;
+
+    public Material material;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float newOffsetX = material.mainTextureOffset.x + scrollSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(newOffsetX, 0);
+
+        material.mainTextureOffset = newOffset;
+    }
+}
